566a0047f5fe67908db2755ff1842966ed5b882e
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 00:54:47 +0530

    Python code to demonstrate working of   isalpha(), isalnum(), isspace()

---
+++ string_functions_in_python/string_function_isalpha_isalnum_isSpace.py
@@ -0,0 +1,19 @@
+# Python code to demonstrate working of 
+# isalpha(), isalnum(), isspace()
+str = "geeksforgeeks"
+str1 = "123abc!"
+  
+# Checking if str has all alphabets 
+if (str.isalpha()):
+       print ("All characters are alphabets in str")
+else : print ("All characters are not alphabets in str")
+ 
+# Checking if str1 has all numbers
+if (str1.isalnum()):
+       print ("All characters are numbers and or alphabets in str1")
+else : print ("All characters are not numbers and or alphabets in str1")
+ 
+# Checking if str1 has all spaces
+if (str1.isspace()):
+       print ("All characters are spaces in str1")
+else : print ("All characters are not spaces in str1");
