f09c62c7c3c1f0fc2aa51680c1a46e8156c28c3f
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 01:06:52 +0530

    len(), min() and max() function in python

---
+++ Lists_in_Python/list_function_len_min_max.py
@@ -0,0 +1,16 @@
+# Python code to demonstrate the working of
+# len(), min() and max()
+# initializing list 1
+lis = [2, 1, 3, 5, 4]
+ 
+# using len() to print length of list
+print ("The length of list is : ", end="")
+print (len(lis))
+ 
+# using min() to print minimum element of list
+print ("The minimum element of list is : ", end="")
+print (min(lis))
+ 
+# using max() to print maximum element of list
+print ("The maximum element of list is : ", end="")
+print (max(lis))
