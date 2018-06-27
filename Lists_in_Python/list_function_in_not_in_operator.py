76617ce96109f34785427a7d4e8ad13eb39ca48e
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 01:05:21 +0530

    "in" and "not in" operator in python

---
+++ Lists_in_Python/list_function_in_not_in_operator.py
@@ -0,0 +1,14 @@
+# Python code to demonstrate the working of
+# "in" and "not in" 
+# initializing list
+lis = [1, 4, 3, 2, 5]
+ 
+# checking if 4 is in list using "in"
+if 4 in lis:
+        print ("List is having element with value 4")
+else :  print ("List is not having element with value 4")
+ 
+# checking if 4 is not list using "not in"
+if 4 not in lis:
+        print ("List is not having element with value 4")
+else :  print ("List is having element with value 4")
