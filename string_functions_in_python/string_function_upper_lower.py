be94eb9fa490bc81425d566e7e72babfa974ac44
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 00:59:05 +0530

    Python code to demonstrate working of upper(), lower(), swapcase() and title()

---
+++ string_functions_in_python/string_function_upper_lower.py
@@ -0,0 +1,19 @@
+# Python code to demonstrate working of 
+# upper(), lower(), swapcase() and title()
+str = "GeeksForGeeks is f0r GeeKs. Have_a_good_DAY. Good Night"
+ 
+# Coverting string into its lower case
+str1 = str.lower();
+print (" The lower case converted string is : " + str1)
+ 
+# Coverting string into its upper case
+str2 = str.upper();
+print (" The upper case converted string is : " + str2)
+ 
+# Coverting string into its swapped case
+str3 = str.swapcase();
+print (" The swap case converted string is : " + str3)
+ 
+# Coverting string into its title case
+str4 = str.title();
+print (" The title case converted string is : " + str4)
