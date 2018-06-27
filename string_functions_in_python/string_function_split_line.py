6850688bf4eb9a68a096b2b1f8559c524fc092e7
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 00:58:08 +0530

    splitline function

---
+++ string_functions_in_python/string_function_split_line.py
@@ -0,0 +1,6 @@
+str = "Line1-a b c d e f\nLine2- a b c\n\nLine4- a b c d";
+print str.splitlines( )
+print str.splitlines( 0 )
+print str.splitlines( 3 )
+print str.splitlines( 4 )
+print str.splitlines( 5 )
