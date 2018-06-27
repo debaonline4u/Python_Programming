93eb31d5f785ae98cc500412a90c659ccaa59ca8
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 00:57:03 +0530

    Python code to demonstrate working of min() and max()

---
+++ string_functions_in_python/string_function_min_max.py
@@ -0,0 +1,11 @@
+# Python code to demonstrate working of 
+# min() and max()
+str = "geeksforgeeks"
+ 
+# using min() to print the smallest character
+# prints 'e'
+print ("The minimum value character is : " + min(str));
+ 
+# using max() to print the largest character
+# prints 's'
+print ("The maximum value character is : " + max(str));
