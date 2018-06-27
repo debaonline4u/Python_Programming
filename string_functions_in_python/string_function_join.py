a3b9c80f3e4472d10ba7e746dd975ba49f6f8009
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 00:55:42 +0530

    Python code to demonstrate working of join()

---
+++ string_functions_in_python/string_function_join.py
@@ -0,0 +1,8 @@
+# Python code to demonstrate working of 
+# join()
+str = "_"
+str1 = ( "geeks", "for", "geeks" )
+ 
+# using join() to join sequence str1 with str
+print ("The string after joining is : ", end="")
+print ( str.join(str1))
