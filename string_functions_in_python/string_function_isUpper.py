f56354e852a7bc4816c7283cc93cf5ac8265e9c4
debaonline4u <debashis.sahoo88@gmail.com>
2018-06-28 00:55:18 +0530

    Python code to demonstrate working of  isupper() and islower()

---
+++ string_functions_in_python/string_function_isUpper.py
@@ -0,0 +1,14 @@
+# Python code to demonstrate working of 
+# isupper() and islower()
+str = "GeeksforGeeks"
+str1 = "geeks"
+ 
+# checking if all characters in str are upper cased
+if str.isupper() :
+       print ("All characters in str are upper cased")
+else : print ("All characters in str are not upper cased")
+ 
+# checking if all characters in str1 are lower cased
+if str1.islower() :
+       print ("All characters in str1 are lower cased")
+else : print ("All characters in str1 are not lower cased")
