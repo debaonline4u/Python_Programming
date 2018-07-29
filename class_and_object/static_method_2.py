bd5ad5dc0f934bd7eb091a91b61ec0a013bb42d4
debaonline4u <debashis.sahoo88@gmail.com>
2018-07-30 00:22:32 +0530

    static method to find square root of the number

---
+++ class_and_object/static_method_2.py
@@ -0,0 +1,14 @@
+# static method to find square root of the number
+
+import math
+class Sample:
+    data = 10
+    @staticmethod
+    def calculate(x):
+        return(math.sqrt(x))
+
+
+# accept number form key board
+
+# number = int(input('Enter Number for square root: \n'))
+# print("Square root of {} is: {:.2f} ".format(number,Sample.calculate(number)))
