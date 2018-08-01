c718625f85360c1ca1983d0936acb68ea5924cef
debashis <debashis.sahoo88@gmail.com>
2018-08-01 11:53:46 +0530

    creating array using arange

---
+++ arrays_in_python/creating_array_using_arange.py
@@ -0,0 +1,19 @@
+# Python program to create an array using arange() function.
+
+from numpy import *
+
+arr = arange(2, 11, 2)
+print(arr)
+
+arr = arange(10)
+print(arr)
+
+arr = arange(1, 10, 3)
+print(arr)
+
+arr = arange(10, 1, -1)
+print(arr)
+
+arr = arange(0, 10, 1.5)
+print(arr)
+
