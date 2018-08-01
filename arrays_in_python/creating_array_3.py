3c7d4d998fd595524ac330286b622eae54512aae
debashis <debashis.sahoo88@gmail.com>
2018-08-01 11:51:52 +0530

    creating_array in python

---
+++ arrays_in_python/creating_array_3.py
@@ -0,0 +1,11 @@
+# creating an array with characters
+
+
+from array import *
+
+arr = array('u', ['d', 'e', 'b', 'a', 's', 'h', 'i', 's'])
+
+print('The array elements are: ')
+for ch in arr:
+    print(ch)
+
