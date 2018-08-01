127f56676c9d3333c38ba3ededf7e6fb9da4530b
debashis <debashis.sahoo88@gmail.com>
2018-08-01 11:54:57 +0530

    creating array using linespace

---
+++ arrays_in_python/creating_array_using_linespace.py
@@ -0,0 +1,9 @@
+# Python program to demonstrate creating array using linespace and logspace.
+
+from numpy import *
+
+arr = linspace(0, 10, 5)
+print('arr = ', arr)
+
+arr2 = logspace(1, 2, 5)
+print('arr2 = ', arr2)
